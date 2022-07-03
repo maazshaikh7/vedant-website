d4ca303697298bdba928fe96383a7f9dd6d7b89a
Matt Wilson <mattwwilson34@gmail.com>
2022-07-03 16:06:59 -0400

    Refactor: isInViewport func
    Refactored isInViewport func to use screen percentages instead of pixel
    values so that it would be more accurate on different size screens.

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,62 @@
+// Check what section is in viewport on page load and set active link class
+window.onload = (event) => {
+  sections.forEach((section, i) => {
+    if (isInViewport(section)) {
+      navLinks[i].classList.add('active--navlink');
+    } else {
+      navLinks[i].classList.remove('active--navlink');
+    }
+  });
+};
+
+// Cash DOM elements
 const navbar = document.getElementById('navbar');
 const navLinks = document.querySelectorAll('.navbar__link');
 const sections = document.querySelectorAll('section');
 
+// Cash navbar height as percent for scroll calculations
 const navRect = navbar.getBoundingClientRect();
-const navHeight = navRect.height;
+const navbarHeightPercent = Math.floor(
+  (navRect.height / window.innerHeight) * 100
+);
 
+console.log(navbarHeightPercent);
+
+// Cash inner window height as a percent for scroll calculations
+const windowHeight = 100;
+
+/**
+ * Checks if DOM element is within the viewport
+ * @param {Object} element DOM element
+ * @returns true/flase
+ */
 const isInViewport = (element) => {
+  //
+  // Get element positional stats
   const rect = element.getBoundingClientRect();
-  const rectTop = rect.top - navHeight;
-  const rectBottom = rect.bottom - navHeight / 2;
 
-  return rectTop >= -window.innerHeight / 2 && rectBottom <= window.innerHeight;
+  // Convert elements top position to percentage
+  const rectTopPercent =
+    Math.floor((rect.top / window.innerHeight) * 100) - navbarHeightPercent;
+
+  // Convert elements bottom position to percentage
+  const rectBottomPercent =
+    Math.floor((rect.bottom / window.innerHeight) * 100) - navbarHeightPercent;
+
+  // If top element top + bottom are out of viewport return false
+  return rectTopPercent <= 5 && rectBottomPercent >= 5;
 };
 
+// If website section in viewport apply/remove active link classes
 window.addEventListener('scroll', (event) => {
   sections.forEach((section, i) => {
-    console.log(`${section.id}`, isInViewport(section));
     if (isInViewport(section)) {
+      //
+      // Add active nav link class if section in window
       navLinks[i].classList.add('active--navlink');
     } else {
+      //
+      // Remove active nav link class if section in window
       navLinks[i].classList.remove('active--navlink');
     }
   });
