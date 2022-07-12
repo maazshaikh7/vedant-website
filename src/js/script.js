2fc48b4d08daa85f8c28249d36204ce70b037a6a
Matt Wilson <mattwwilson34@gmail.com>
2022-07-11 21:17:25 -0400

    Refactor: added iterator to hamburgerMenu lines

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,12 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const lines = document.querySelectorAll('.line');
 
 hamburgerMenu.addEventListener('click', (event) => {
-  const [line1, line2, line3] = lines;
-
   // Apply classes on click
   if (hamburgerMenu.classList.contains('open')) {
     hamburgerMenu.classList.remove('open');
-    line1.classList.remove('line1-open-menu');
-    line2.classList.remove('line2-open-menu');
-    line3.classList.remove('line3-open-menu');
+    lines.forEach((line, i) => line.classList.remove(`line${i + 1}-open-menu`));
   } else {
     hamburgerMenu.classList.add('open');
-    line1.classList.add('line1-open-menu');
-    line2.classList.add('line2-open-menu');
-    line3.classList.add('line3-open-menu');
+    lines.forEach((line, i) => line.classList.add(`line${i + 1}-open-menu`));
   }
 });
