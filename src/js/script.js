98a21e78873df8f7971363fdd392a4bee63d44e5
Matt Wilson <49503056+Mattwwilson34@users.noreply.github.com>
2022-07-12 15:03:26 -0400

    Add: anchor tag mobile menu close on click

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const navLinkContainer = document.querySelector('.nav-link-container')
+const navlinks = document.querySelectorAll('.nav-link-container > a')
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const lines = document.querySelectorAll('.line');
 
@@ -14,3 +15,9 @@ hamburgerMenu.addEventListener('click', (event) => {
     lines.forEach((line, i) => line.classList.add(`line${i + 1}-open-menu`));
   }
 });
+
+navlinks.forEach(link => link.addEventListener('click', (event) => {
+  navLinkContainer.style.cssText = 'height: 0; margin: 0;'
+  hamburgerMenu.classList.remove('open');
+  lines.forEach((line, i) => line.classList.remove(`line${i + 1}-open-menu`));
+}))
