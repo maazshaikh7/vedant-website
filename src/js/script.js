66d3de91aa193e640329dd41f6f0da8e3800e79c
Matt Wilson <mattwwilson34@gmail.com>
2022-07-14 19:50:21 -0400

    Refactor: change link height from vh to rem

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ hamburgerMenu.addEventListener('click', (event) => {
     hamburgerMenu.classList.remove('open');
     lines.forEach((line, i) => line.classList.remove(`line${i + 1}-open-menu`));
   } else {
-    navLinkContainer.style.cssText = 'height: 25vh; margin: 1rem 0 0 0;';
+    navLinkContainer.style.cssText = 'height: 14rem; margin: 1rem 0 0 0;';
     hamburgerMenu.classList.add('open');
     lines.forEach((line, i) => line.classList.add(`line${i + 1}-open-menu`));
   }
