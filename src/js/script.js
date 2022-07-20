3347423d10cc5464c85c4456db9cc20650e67286
Matt Wilson <49503056+Mattwwilson34@users.noreply.github.com>
2022-07-20 13:59:45 -0400

    Add: fix nav-link-container margin on click
    When clicking on navigation links on screen sizes > 768px margin would be set to 0 causing a visible jump. Fixed with a check screen width conditional.

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@ const navlinks = document.querySelectorAll('.nav-link-container > a');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const lines = document.querySelectorAll('.line');
 
+console.log(window.innerWidth);
+
 hamburgerMenu.addEventListener('click', (event) => {
   // Apply classes on click
   if (hamburgerMenu.classList.contains('open')) {
@@ -10,16 +12,18 @@ hamburgerMenu.addEventListener('click', (event) => {
     hamburgerMenu.classList.remove('open');
     lines.forEach((line, i) => line.classList.remove(`line${i + 1}-open-menu`));
   } else {
-    navLinkContainer.style.cssText = 'height: 14rem; margin: 1rem 0 0 0;';
+    navLinkContainer.style.cssText = 'height: 18rem; margin: 1rem 0 0 0;';
     hamburgerMenu.classList.add('open');
     lines.forEach((line, i) => line.classList.add(`line${i + 1}-open-menu`));
   }
 });
 
 navlinks.forEach((link) =>
-  link.addEventListener('click', (event) => {
-    navLinkContainer.style.cssText = 'height: 0; margin: 0;';
-    hamburgerMenu.classList.remove('open');
-    lines.forEach((line, i) => line.classList.remove(`line${i + 1}-open-menu`));
+  link.addEventListener('click', () => {
+    if (window.innerWidth < 768) {
+      navLinkContainer.style.cssText = 'height: 0; margin: 0;';
+      hamburgerMenu.classList.remove('open');
+      lines.forEach((line, i) => line.classList.remove(`line${i + 1}-open-menu`));
+    }
   })
 );
