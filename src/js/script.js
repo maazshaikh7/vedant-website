6360b586605bdf0dc0a853906d75a5076b120fbc
Matt Wilson <mattwwilson34@gmail.com>
2023-03-07 10:33:36 -0500

    feat(github-links): stop event propagation

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,28 @@
-const navLinkContainer = document.querySelector('.nav-link-container');
-const navlinks = document.querySelectorAll('.nav-link-container > a');
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const lines = document.querySelectorAll('.line');
-const projectLinks = document.querySelectorAll('#project-external-link');
+const navLinkContainer = document.querySelector(".nav-link-container");
+const navlinks = document.querySelectorAll(".nav-link-container > a");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const lines = document.querySelectorAll(".line");
+const githubLinks = document.querySelectorAll(".project-icons > a");
+console.log(githubLinks);
 
-hamburgerMenu.addEventListener('click', (event) => {
+hamburgerMenu.addEventListener("click", (event) => {
   // Apply classes on click
-  if (hamburgerMenu.classList.contains('open')) {
-    navLinkContainer.style.cssText = 'height: 0; margin: 0;';
-    hamburgerMenu.classList.remove('open');
+  if (hamburgerMenu.classList.contains("open")) {
+    navLinkContainer.style.cssText = "height: 0; margin: 0;";
+    hamburgerMenu.classList.remove("open");
     lines.forEach((line, i) => line.classList.remove(`line${i + 1}-open-menu`));
   } else {
-    navLinkContainer.style.cssText = 'height: 20rem; margin: 1rem 0 0 0;';
-    hamburgerMenu.classList.add('open');
+    navLinkContainer.style.cssText = "height: 20rem; margin: 1rem 0 0 0;";
+    hamburgerMenu.classList.add("open");
     lines.forEach((line, i) => line.classList.add(`line${i + 1}-open-menu`));
   }
 });
 
 navlinks.forEach((link) =>
-  link.addEventListener('click', () => {
+  link.addEventListener("click", () => {
     if (window.innerWidth < 768) {
-      navLinkContainer.style.cssText = 'height: 0; margin: 0;';
-      hamburgerMenu.classList.remove('open');
+      navLinkContainer.style.cssText = "height: 0; margin: 0;";
+      hamburgerMenu.classList.remove("open");
       lines.forEach((line, i) =>
         line.classList.remove(`line${i + 1}-open-menu`)
       );
@@ -29,22 +30,16 @@ navlinks.forEach((link) =>
   })
 );
 
-projectLinks.forEach((link) =>
-  link.addEventListener('click', (e) => {
-    alert('Project Demo Coming Soon!');
-  })
-);
-
-
-// test
-const containers = document.querySelectorAll(".project-container");
-containers.forEach(container => {
-  container.addEventListener("click", function() {
-    this.nextElementSibling.style.display =
-      this.nextElementSibling.style.display === "block"
-        ? "none"
-        : "block";
+githubLinks.forEach((githubLink) => {
+  githubLink.addEventListener("click", (event) => {
+    event.stopPropagation();
   });
 });
 
-
+const containers = document.querySelectorAll(".project-container");
+containers.forEach((container) => {
+  container.addEventListener("click", function () {
+    this.nextElementSibling.style.display =
+      this.nextElementSibling.style.display === "block" ? "none" : "block";
+  });
+});
